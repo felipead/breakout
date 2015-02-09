65fc1d48e5c619f551bd51b6735227a1869a002a
Felipe Dornelas <felipead@gmail.com>
2015-02-09 00:46:07 -0200

    Added more test coverage to Rectangle.

--- source/test/geometry/TestRectangle.py
+++ source/test/geometry/TestRectangle.py
@@ -2,7 +2,16 @@
 from breakout.geometry.Rectangle import Rectangle
 
 
+# noinspection PyMethodMayBeStatic
 class TestRectangle:
+
+    def test_get_coordinates(self):
+        left, bottom, right, top = -10, -10, 10, 10
+        rectangle = Rectangle(left = left, bottom = bottom, right = right, top = top)
+        assert rectangle.left == left
+        assert rectangle.right == right
+        assert rectangle.bottom == bottom
+        assert rectangle.top == top
 
     def test_width(self):
         rectangle = Rectangle(left = -10, bottom = -10, right = 10, top = 10)
